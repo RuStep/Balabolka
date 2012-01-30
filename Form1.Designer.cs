2ba07e5678913a7f0af3b8da5695fdbfa6e5825d
Rustep <RuStep23@ayndex.ru>
2012-01-30 23:36:56 +0300

    Updated with SnoUweR's commit and was added info about friend

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,35 +35,46 @@
             this.tStrip1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.button1 = new System.Windows.Forms.Button();
             this.listF = new System.Windows.Forms.ListBox();
-            this.listH = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listH = new System.Windows.Forms.ListView();
+            this.col1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // listId
             // 
+            this.listId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listId.FormattingEnabled = true;
-            this.listId.Location = new System.Drawing.Point(13, 13);
+            this.listId.Location = new System.Drawing.Point(13, 135);
             this.listId.Name = "listId";
-            this.listId.Size = new System.Drawing.Size(137, 238);
+            this.listId.Size = new System.Drawing.Size(137, 236);
             this.listId.TabIndex = 0;
             this.listId.SelectedIndexChanged += new System.EventHandler(this.listF_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(156, 230);
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(156, 319);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(335, 20);
+            this.textBox1.Size = new System.Drawing.Size(378, 50);
             this.textBox1.TabIndex = 1;
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tStrip1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 258);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 384);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(551, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(547, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -74,36 +85,36 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(497, 227);
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Webdings", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(486, 319);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(40, 23);
+            this.button1.Size = new System.Drawing.Size(50, 50);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "]";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listF
             // 
+            this.listF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listF.FormattingEnabled = true;
-            this.listF.Location = new System.Drawing.Point(13, 13);
+            this.listF.Location = new System.Drawing.Point(13, 122);
             this.listF.Name = "listF";
-            this.listF.Size = new System.Drawing.Size(137, 238);
+            this.listF.Size = new System.Drawing.Size(137, 236);
             this.listF.TabIndex = 4;
-            // 
-            // listH
-            // 
-            this.listH.FormattingEnabled = true;
-            this.listH.Location = new System.Drawing.Point(159, 26);
-            this.listH.Name = "listH";
-            this.listH.Size = new System.Drawing.Size(378, 199);
-            this.listH.TabIndex = 5;
+            this.listF.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(156, 10);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(245, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(59, 19);
             this.label1.TabIndex = 6;
             this.label1.Text = "label1";
             // 
@@ -111,23 +122,91 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // listH
+            // 
+            this.listH.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listH.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col1,
+            this.col2});
+            this.listH.GridLines = true;
+            this.listH.Location = new System.Drawing.Point(156, 135);
+            this.listH.Name = "listH";
+            this.listH.Size = new System.Drawing.Size(380, 187);
+            this.listH.TabIndex = 7;
+            this.listH.UseCompatibleStateImageBehavior = false;
+            this.listH.View = System.Windows.Forms.View.Tile;
+            // 
+            // col1
+            // 
+            this.col1.Width = 10;
+            // 
+            // col2
+            // 
+            this.col2.Width = 100;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(159, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(249, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 14);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(249, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 14);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(156, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 14);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 280);
+            this.ClientSize = new System.Drawing.Size(547, 406);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listH);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listId);
             this.Controls.Add(this.listF);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,9 +220,14 @@
         private System.Windows.Forms.ToolStripStatusLabel tStrip1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listF;
-        private System.Windows.Forms.ListBox listH;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listH;
+        private System.Windows.Forms.ColumnHeader col1;
+        private System.Windows.Forms.ColumnHeader col2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -6,28 +6,36 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 using ApiCore;
 using ApiCore.Friends;
 using ApiCore.Messages;
-using System.Xml;
+
 
 namespace Balabolka
 {
     public partial class Form1 : Form
     {
-        private bool isLoggedIn = false;
+        private bool _isLoggedIn = false;
 
-        private SessionInfo sessionInfo;
-        private ApiManager manager;
-        private MessagesFactory messagesFactory;
-        private FriendsFactory friendsFactory;
-        private List<Friend> friendsList;
-        private List<ApiCore.Messages.Message> messagesList;
-        public string mid;
-        private int uid;
-        private int my_id;
-        private string fname;
-        private string lname;
+        private SessionInfo _sessionInfo;
+        private ApiManager _manager;
+        private MessagesFactory _messagesFactory;
+        private FriendsFactory _friendsFactory;
+        private List<Friend> _friendsList;
+        private List<ApiCore.Messages.Message> _messagesList;
+        public string Mid;
+        private int _myId;
+        private string _fname;
+        private string _lname;
+        private string _nname;
+        private string _avaurl;
+        private string _faculty_name;
+        private string _university_name;
+        private string _bdate;
+        private bool _online;
+
+
 
         public Form1()
         {
@@ -42,26 +50,26 @@ namespace Balabolka
 
         private void Reauth()
         {
-            if (isLoggedIn != true)
+            if (_isLoggedIn != true)
             {
                 SessionManager sm = new SessionManager(2774221, Convert.ToInt32(ApiPerms.Audio | ApiPerms.ExtendedMessages | ApiPerms.ExtendedWall | ApiPerms.Friends | ApiPerms.Offers | ApiPerms.Photos | ApiPerms.Questions | ApiPerms.SendNotify | ApiPerms.SidebarLink | ApiPerms.UserNotes | ApiPerms.UserStatus | ApiPerms.Video | ApiPerms.WallPublisher | ApiPerms.Wiki));
                 //sm.Log += new SessionManagerLogHandler(sm_Log);
-                sessionInfo = sm.GetSession();
-                my_id = int.Parse(sessionInfo.MemberId);
+                _sessionInfo = sm.GetSession();
+                _myId = int.Parse(_sessionInfo.MemberId);
 
-                if (sessionInfo != null)
+                if (_sessionInfo != null)
                 {
-                    isLoggedIn = true;
+                    _isLoggedIn = true;
                 }
                 Reauth();
             }
 
-            if (isLoggedIn)
+            if (_isLoggedIn)
             {
-                manager = new ApiManager(sessionInfo);
+                _manager = new ApiManager(_sessionInfo);
                 //manager.Log += new ApiManagerLogHandler(manager_Log);
                 //manager.DebugMode = true;
-                manager.Timeout = 10000;
+                _manager.Timeout = 10000;
                 tStrip1.Text = "Онлайн";
                 GetFriends();
             }
@@ -69,55 +77,103 @@ namespace Balabolka
 
         private void Send(int id, string message)
         {
-            messagesFactory = new MessagesFactory(manager);
-            messagesFactory.Send(id, message, null, SendMessageType.FromChat);
+            _messagesFactory = new MessagesFactory(_manager);
+            _messagesFactory.Send(id, message, null, SendMessageType.FromChat);
         }
 
         private void GetFriends()
         {
-            friendsFactory = new FriendsFactory(manager);
+            _friendsFactory = new FriendsFactory(_manager);
             string[] fields = { "uid", "first_name", "last_name" };
-            friendsList = friendsFactory.Get("nom", null, 0, null, fields);
+            _friendsList = _friendsFactory.Get("nom", null, 0, null, fields);
 
-            foreach (Friend a in friendsList)
+            foreach (Friend a in _friendsList)
             {
                 listF.Items.Add(String.Concat(a.Id));
                 listId.Items.Add(String.Concat(a.FirstName + " " + a.LastName));
             }
         }
-        private void GetName()
+        private void GetFriend()
         {
-            manager.Method("getProfiles");
-            manager.Params("uids", mid);
-            manager.Params("fields", "first_name, last_name");
+            _manager.Method("getProfiles");
+            _manager.Params("uids", Mid);
+            _manager.Params("fields", "first_name, last_name, photo_medium, nickname, education, bdate, online");
 
-            XmlNode result = manager.Execute().GetResponseXml().FirstChild;
+            XmlNode result = _manager.Execute().GetResponseXml().FirstChild;
             XmlUtils.UseNode(result);
-            fname = XmlUtils.String("first_name");
-            lname = XmlUtils.String("last_name");
-            label1.Text = "Ваш собеседник - " + fname + " " + lname;
+            _fname = XmlUtils.String("first_name");
+            _lname = XmlUtils.String("last_name");
+            _avaurl = XmlUtils.String("photo_medium");
+            _nname = XmlUtils.String("nickname");
+            _university_name = XmlUtils.String("university_name");
+            _faculty_name = XmlUtils.String("faculty_name");
+            _bdate = XmlUtils.String("bdate");
+            _online = XmlUtils.Bool("online");
+
+            pictureBox1.ImageLocation = _avaurl;
+            label1.Text = _fname + " " + _nname + " " + _lname;
+            label3.Text = _bdate;
+
+            if (_university_name == null)
+            {
+                label2.Text = _university_name + ", " + _faculty_name;
+            }
+            else
+            {
+                label2.Text = _bdate;
+                label3.Text = "";
+            }
+
+            if (_online == true)
+            {
+                label4.Text = "В сети";
+                label4.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                label4.Text = "Не в сети";
+                label4.ForeColor = Color.DarkRed;
+            }
         }
 
         private void GetHistory()
         {
-            messagesFactory = new MessagesFactory(manager);
-            messagesList = messagesFactory.GetHistory(Convert.ToInt32(mid), null, 50);
+            _messagesFactory = new MessagesFactory(_manager);
+            _messagesList = _messagesFactory.GetHistory(Convert.ToInt32(Mid), null, 50);
 
-            if (messagesList != null)
+            if (_messagesList != null)
             {
-                foreach (ApiCore.Messages.Message a in messagesList)
+                foreach (ApiCore.Messages.Message a in _messagesList)
                 {
                     if (a.Body != null)
                     {
-                        if (a.UserId == Convert.ToInt32(mid))
+                        if (a.UserId == Convert.ToInt32(Mid))
                         {
-                            listH.Items.Add(String.Concat(fname + ":"));
+                            //listH.Items.Add(String.Concat(_fname + ":"));
+                            col1.ListView.Items.Add(String.Concat(_fname + ":"));
                         }
                         else
                         {
-                            listH.Items.Add("Я:");
+                            col1.ListView.Items.Add("Я:");
+                            //listH.Items.Add("Я:");
                         }
-                        listH.Items.Add(String.Concat("    " + a.Body));
+                        //listH.Items.Add(String.Concat("    " + a.Body));
+                        //listH.Items.Add(a.Body);
+                        col2.ListView.Items.Add(a.Body);
+                    }
+                }
+
+                for (int i = 0; i < listH.Items.Count; i++)
+                {
+                    // 0 - это автор
+                    // 1 - это сообщение
+                    if (i == 0 || i % 2 == 0)
+                    {
+                        listH.Items[i].ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        listH.Items[i].ForeColor = Color.Black;
                     }
                 }
             }
@@ -131,22 +187,25 @@ namespace Balabolka
         private void listF_SelectedIndexChanged(object sender, EventArgs e)
         {
             listF.SelectedIndex = listId.SelectedIndex;
-            mid = listF.SelectedItem.ToString();
+            Mid = listF.SelectedItem.ToString();
             listH.Items.Clear();
-            GetName();
+            GetFriend();
             GetHistory();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Send(Convert.ToInt32(mid), textBox1.Text);
-            textBox1.Text = "";
+            if (textBox1.Text != null && textBox1.Text != " ")
+            {
+                Send(Convert.ToInt32(Mid), textBox1.Text);
+                textBox1.Text = "";
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             GetHistory();
-            GetName();
+            GetFriend();
             GetFriends();
         }
 
